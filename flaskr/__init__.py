feba0dc4e8391594912f86c295cf4b651f667b3a
shubhMaheshwari <maheshwarishubh98@gmail.com>
2018-08-24 15:32:39 +0530

    Basic Homepage

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,8 +1,43 @@
-def create_app():
-    app = ...
-    # existing code omitted
+import os
+from flask import Flask,Blueprint, render_template
+import json
+from . import db
 
-    from . import db
-    db.init_app(app)
+def create_app(test_config=None):
+	# create and configure the app
+	app = Flask(__name__, instance_relative_config=True)
+	app.config.from_mapping(
+		SECRET_KEY='dev',
+		DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+	)
+	db.init_app(app)
 
-    return app
+
+	if test_config is None:
+		# load the instance config, if it exists, when not testing
+		app.config.from_pyfile('config.py', silent=True)
+	else:
+		# load the test config if passed in
+		app.config.from_mapping(test_config)
+
+	# ensure the instance folder exists
+	try:
+		os.makedirs(app.instance_path)
+	except OSError:
+		pass
+
+	# a simple page that says hello
+	@app.route('/')
+	def Homepage():
+		with open('./flaskr/static/basic.json' , 'r') as f:
+			data = json.load(f)
+
+		return render_template('index.html', tabs=data['tabs'], emotions=data['emotions'], star_color=data['star_color'], task_list=data['task_list'],hormones=data['hormones'])
+
+	return app
+
+
+if __name__ == "__main__":
+	app = create_app()
+	app.config['TEMPLATES_AUTO_RELOAD']=True
+	app.run(debug=True,use_reloader=True)	
--- run.py
+++ run.py
@@ -1,51 +1,9 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import os
+from flaskr import create_app
 
-@app.route('/')
-def App():
-
-	tabs = [
-		{'id': 'hormones',
-		'name': 'Hormones'},
-		{'id': 'tasks',
-		'name': 'Tasks'},
-		{'id': 'emotions',
-		'name': 'Emotions'}
-		]
-
-	emotions = [{'name': 'Trust',
-		'value' : 5},
-		{'name': 'Confidence',
-		'value' : 5},
-		{'name': 'Reward',
-		'value' : 5},
-		{'name': 'Sleep',
-		'value' : 5},
-		{'name': 'Energy',
-		'value' : 5},
-		{'name': 'Stress',
-		'value' : 5},
-		{'name': 'Money',
-		'value' : 5}
-		]
-
-	task_list = [{'name': 'Sleep 8 hours', 'periodic':True, 'periodic_time': '24 hours'},
-		{'name': 'DIP Assignment 2', 'periodic':False, 'deadline': '12:00' },
-		{'name': 'CP', 'periodic':True, 'periodic_time': '12 hours'}
-		]
-
-	star_color = ['red' , 'red', 'red', 'yellow', 'yellow', 'yellow', 'blue' , 'blue', 'green' , 'green']
-
-	hormones=[{'name' : 'Dopamine' , "data": ' '.join([str(x) for x in range(8) ])},
-		{'name' : 'Seretonine' , "data": ' '.join([str(x) for x in range(8) ])},
-		{'name' : 'Oxytocin' , "data": ' '.join([str(x) for x in range(8) ])},
-		{'name' : 'Aderline' , "data": ' '.join([str(x) for x in range(8) ])},
-		{'name' : 'Cortisol/Stress' , "data": ' '.join([str(x) for x in range(8) ])}]
-	print(hormones[0])
-
-	return render_template('index.html', tabs=tabs, emotions=emotions, star_color=star_color, task_list=task_list,hormones=hormones)
 
 
 if __name__ == "__main__":
-    app.config['TEMPLATES_AUTO_RELOAD']=True
-    app.run(debug=True,use_reloader=True)	
+	app = create_app()
+	app.config['TEMPLATES_AUTO_RELOAD']=True
+	app.run(debug=True,use_reloader=True)	
